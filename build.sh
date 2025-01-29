3309f5ca47b65cf1af2dd4655ded5136aa074206
Kneba <abenkenary3@gmail.com>
2025-01-29 22:41:41 +0700

    asdasd

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-
 #
 # Copyright (C) 2023-2024 Kneba <abenkenary3@gmail.com>
 #
@@ -33,14 +32,13 @@ GCCaPath="${MainPath}/GCC64"
 GCCbPath="${MainPath}/GCC32"
 
 # Identity
-KERNELNAME=TOM
-CODENAME=X00TD
+KERNELNAME=TheOneMemory
+CODENAME=Nothing
 VARIANT=EAS
-VERSION=STBL
-BASE=android-4.19-stable
+BASE=android13-4.19-sdm660
 
 # The name of the Kernel, to name the ZIP
-ZIPNAME="$KERNELNAME-$CODENAME-$VERSION"
+ZIPNAME="$KERNELNAME-UDC-4-19-315"
 
 # Show manufacturer info
 MANUFACTURERINFO="ASUSTek Computer Inc."
@@ -48,7 +46,7 @@ MANUFACTURERINFO="ASUSTek Computer Inc."
 # Clone Kernel Source
 echo " "
 msg "|| Cloning Kernel Source ||"
-git clone --depth=1 --recursive https://github.com/Tiktodz/android_kernel_asus_sdm660 -b master kernel
+git clone --depth=1 --recursive https://$USERNAME:$TOKEN@github.com/Tiktodz/android_kernel_asus_sdm660 -b master kernel
 
 # Clone AOSP Clang
 [[ "$(pwd)" != "${MainPath}" ]] && cd "${MainPath}"
@@ -56,50 +54,47 @@ rm -rf $ClangPath/*
 mkdir $ClangPath
 
 msg "|| Cloning AOSP Clang ||"
+#git clone --depth=1 https://gitlab.com/ImSurajxD/clang-r450784d -b master $ClangPath
+https://gitlab.com/magchuzPM/clang-r498229b.git -b master $ClangPath
 #wget -q https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/master/clang-r498229b.tar.gz -O "clang-r498229b.tar.gz"
 #tar -xf clang-r498229b.tar.gz -C $ClangPath
-wget -q https://github.com/Tiktodz/electrowizard-clang/releases/download/ElectroWizard-Clang-18.1.8-release/ElectroWizard-Clang-18.1.8.tar.gz -O "ElectroWizard-Clang-18.1.8.tar.gz"
-tar -xf ElectroWizard-Clang-18.1.8.tar.gz -C $ClangPath
-rm -rf ElectroWizard-Clang-18.1.8.tar.gz
+#wget -q https://github.com/ftrsndrya/ElectroWizard-Clang/releases/download/ElectroWizard-Clang-19.0.0-release/ElectroWizard-Clang-19.0.0.tar.gz -O "ElectroWizard-Clang-19.0.0.tar.gz"
+#tar -xf ElectroWizard-Clang-19.0.0.tar.gz -C $ClangPath
 
 # Clone GCC
-#rm -rf $GCCaPath/*
-#rm -rf $GCCbPath/*
-#mkdir $GCCaPath
-#mkdir $GCCbPath
-#msg "|| Cloning AOSP GCC ||"
-#wget -q https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/+archive/refs/tags/android-12.1.0_r27.tar.gz -O "gcc64.tar.gz"
-#tar -xf gcc64.tar.gz -C $GCCaPath
-#wget -q https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9/+archive/refs/tags/android-12.1.0_r27.tar.gz -O "gcc32.tar.gz"
-#tar -xf gcc32.tar.gz -C $GCCbPath
+rm -rf $GCCaPath/*
+rm -rf $GCCbPath/*
+mkdir $GCCaPath
+mkdir $GCCbPath
+msg "|| Cloning AOSP GCC ||"
+wget -q https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/+archive/refs/tags/android-12.1.0_r27.tar.gz -O "gcc64.tar.gz"
+tar -xf gcc64.tar.gz -C $GCCaPath
+wget -q https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9/+archive/refs/tags/android-12.1.0_r27.tar.gz -O "gcc32.tar.gz"
+tar -xf gcc32.tar.gz -C $GCCbPath
 
 ##------------------------------------------------------##
 ##---------Do Not Touch Anything Beyond This------------##
 
 # Prepared
 KERNEL_ROOTDIR=$(pwd)/kernel # IMPORTANT ! Fill with your kernel source root directory.
-export ARCH=arm64
-export SUBARCH=arm64
-#export LD=ld.lld
-#export HOSTLD=ld.lld
-#export CCACHE=1
-export LLVM=1
-export LLVM_IAS=1
+export LD=ld.lld
+export HOSTLD=ld.lld
+export CCACHE=1
 export KBUILD_BUILD_USER=queen # Change with your own name or else.
 export KBUILD_BUILD_HOST=github-actions # Change with your own host name or else.
 IMAGE=$KERNEL_ROOTDIR/out/arch/arm64/boot/Image.gz-dtb
 CLANG_VER="$("$ClangPath"/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')"
 LLD_VER="$("$ClangPath"/bin/ld.lld --version | head -n 1)"
 export KBUILD_COMPILER_STRING="$CLANG_VER with $LLD_VER"
-ClangMoreStrings="AR=llvm-ar NM=llvm-nm AS=llvm-as STRIP=llvm-strip OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump READELF=llvm-readelf HOSTAR=llvm-ar HOSTAS=llvm-as LD_LIBRARY_PATH=$ClangPath/lib LD=ld.lld HOSTLD=ld.lld"
-LD_LIBRARY_PATH="${ClangPath}/lib64:${LD_LIBRARY_PATH}"
-PATH=$ClangPath/bin:${PATH}
 DATE=$(TZ=Asia/Jakarta date +"%Y%m%d-%H%M")
 DATE2=$(TZ=Asia/Jakarta date +"%Y%m%d")
 START=$(date +"%s")
 
 # Java
 command -v java > /dev/null 2>&1
+
+# Check Kernel Version
+KERVER=$(cd $KERNEL_ROOTDIR; make kernelversion)
 
 # Telegram
 export BOT_MSG_URL="https://api.telegram.org/bot$TG_TOKEN/sendMessage"
@@ -111,10 +106,10 @@ tg_post_build() {
 
     #Show the Checksum alongwith caption
     curl --progress-bar -F document=@"$1" "$BOT_BUILD_URL" \
-    -F chat_id="TG_CHAT_ID"  \
+    -F chat_id="$2"  \
     -F "disable_web_page_preview=true" \
     -F "parse_mode=html" \
-    -F caption="$2 | <b>MD5 Checksum : </b><code>$MD5CHECK</code>"
+    -F caption="$3 | <b>MD5 Checksum : </b><code>$MD5CHECK</code>"  
 }
 
 # Telegram messaging
@@ -129,23 +124,31 @@ make="./makeparallel"
 # Compiler
 compile(){
 cd ${KERNEL_ROOTDIR}
-# Check Kernel Version
-KERVER=$(make kernelversion)
 export HASH_HEAD=$(git rev-parse --short HEAD)
 export COMMIT_HEAD=$(git log --oneline -1)
 msg "|| Compile starting ||"
-make -j$(nproc) O=out ARCH=arm64 asus/X00TD_defconfig "${MAKE[@]}" 2>&1 | tee error.log
+make -j$(nproc) O=out ARCH=arm64 vendor/X00TD_defconfig
 make -j$(nproc) ARCH=arm64 O=out \
-    ARCH=$ARCH \
-    SUBARCH=$ARCH \
-    CC="$ClangPath/bin/clang" \
-    CROSS_COMPILE=aarch64-linux-gnu- \
-    HOSTCC="$ClangPath/bin/clang" \
-    HOSTCXX="$ClangPath/bin/clang++" ${ClangMoreStrings} "${MAKE[@]}" 2>&1 | tee error.log
+    LD_LIBRARY_PATH="${ClangPath}/lib64:${LD_LIBRARY_PATH}" \
+    PATH=$ClangPath/bin:$GCCaPath/bin:$GCCbPath/bin:/usr/bin:${PATH} \
+    CC=${ClangPath}/bin/clang \
+    NM=${ClangPath}/bin/llvm-nm \
+    CXX=${ClangPath}/bin/clang++ \
+    AR=${ClangPath}/bin/llvm-ar \
+    STRIP=${ClangPath}/bin/llvm-strip \
+    OBJCOPY=${ClangPath}/bin/llvm-objcopy \
+    OBJDUMP=${ClangPath}/bin/llvm-objdump \
+    OBJSIZE=${ClangPath}/bin/llvm-size \
+    READELF=${ClangPath}/bin/llvm-readelf \
+    CROSS_COMPILE=aarch64-linux-android- \
+    CROSS_COMPILE_ARM32=arm-linux-androideabi- \
+    CLANG_TRIPLE=aarch64-linux-gnu- \
+    HOSTAR=${ClangPath}/bin/llvm-ar \
+    HOSTCC=${ClangPath}/bin/clang \
+    HOSTCXX=${ClangPath}/bin/clang++
 
    if ! [ -a "$IMAGE" ]; then
-	finerr "error.log" "Compile Error!!"
-	msg "|| Compile Failed!!! ||"
+	finerr
 	exit 1
    fi
    git clone --depth=1 https://github.com/Tiktodz/AnyKernel3 -b 419 AnyKernel
@@ -173,14 +176,13 @@ function push() {
 }
 # Find Error
 function finerr() {
-    curl -F document=@"$1" "$BOT_MSG_URL" \
-	    -F chat_id="$TG_CHAT_ID"  \
-	    -F "disable_web_page_preview=true" \
-	    -F "parse_mode=Markdown" \
-	    -F caption="$2"
+    curl -s -X POST "$BOT_MSG_URL" \
+        -d chat_id="$TG_CHAT_ID" \
+        -d "disable_web_page_preview=true" \
+        -d "parse_mode=markdown" \
+        -d text="❌ Tetap menyerah...Pasti bisa!!!"
     exit 1
 }
-
 # Zipping
 function zipping() {
 	cd AnyKernel || exit 1
