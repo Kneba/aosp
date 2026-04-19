007227c6ac0c4f74fdfa57dc68e1f76ec05ccaa6
texascake <tiktodz@soulvibe-team.cloud>
2026-04-19 20:14:30 +0700

    Toggle wget commands for Clang versions

--- build.sh
+++ build.sh
@@ -62,12 +62,12 @@ msg "|| Cloning AOSP Clang ||"
 #tar -xf clang-r584948.tar.gz -C $ClangPath
 
 ## clang 21 ##
-wget -q https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/refs/heads/mirror-goog-main-llvm-toolchain-source/clang-r563880c.tar.gz -O "clang-r563880c.tar.gz"
-tar -xf clang-r563880c.tar.gz -C $ClangPath
+#wget -q https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/refs/heads/mirror-goog-main-llvm-toolchain-source/clang-r563880c.tar.gz -O "clang-r563880c.tar.gz"
+#tar -xf clang-r563880c.tar.gz -C $ClangPath
 
 ## clang 20 ##
-#wget -q https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/refs/heads/main/clang-r547379.tar.gz -O "clang-r547379.tar.gz"
-#tar -xf clang-r547379.tar.gz -C $ClangPath
+wget -q https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/refs/heads/main/clang-r547379.tar.gz -O "clang-r547379.tar.gz"
+tar -xf clang-r547379.tar.gz -C $ClangPath
 #git clone --depth=1 https://gitlab.com/kei-space/clang/r547379 $ClangPath
 
 ## clang 18 ##
